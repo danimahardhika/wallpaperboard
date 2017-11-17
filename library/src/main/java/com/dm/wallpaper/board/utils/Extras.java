560a05ebbde577d6f9819cc538f96006e2f41d74
Dani Mahardhika <danimahardhika112@gmail.com>
2017-11-17 14:07:29 +0700

    Update Extras.java

--- library/src/main/java/com/dm/wallpaper/board/utils/Extras.java
+++ library/src/main/java/com/dm/wallpaper/board/utils/Extras.java
@@ -28,7 +28,7 @@ public abstract class Extras {
     public static final String TAG_FAVORITES = "favorites";
     public static final String TAG_SETTINGS = "settings";
     public static final String TAG_ABOUT = "about";
-    
+
     public static final String EXTRA_URL = "url";
     public static final String EXTRA_POSITION = "position";
     public static final String EXTRA_IMAGE = "image";
@@ -36,6 +36,7 @@ public abstract class Extras {
     public static final String EXTRA_FRAGMENT_ID = "fragmentId";
     public static final String EXTRA_CATEGORY = "category";
     public static final String EXTRA_COUNT = "count";
+    public static final String EXTRA_ITEM = "item";
 
     public static final String EXTRA_KEY = "key";
     public static final String EXTRA_PRODUCT_ID = "productId";
@@ -47,5 +48,4 @@ public abstract class Extras {
 
     public static final int ID_CATEGORY_WALLPAPERS = 0;
     public static final int ID_WALLPAPER_SEARCH = 1;
-
 }
